dcc6bd6366acdd1921d9c6749ce3dd50b9f687d6
NatiDeme <demelashnatnael@hotmail.com>
2023-01-24 16:26:20 +0300

    Add landing layout, custom colors and fontfamily

--- src/components/landingmain.jsx
+++ src/components/landingmain.jsx
@@ -3,37 +3,45 @@ import React from "react";
 export default function Landingmain() {
   return (
     <div>
-      <div>
-        <div>
-          <p className="text-7xl w-[28rem] font-black">
-            Changing your eating habits
+      <div className="bg-[#0e1f44]">
+        <div className="bg-gradient-to-r from-brand-blue via-brand-neutral to-brand-neutral rounded-2xl bg-opacity-50">
+          <p className="text-7xl w-[28rem] font-black text-white">Register</p>
+          <p className="text-white">
+            <span className="text-[#e87206]">Free</span> subscription 7 days
           </p>
-          <p className="w-[22rem]">
-            Always take care of your health starting from the food menu that you
-            consume everyday
+          <p className="w-96">
+            Games,Streams,Music,Movies all in on Platforms!
           </p>
           <div className="flex gap-16 items-center">
-            <button className="bg-yellow-400 p-3 rounded-lg">
-              Explore Menu
+            <button className="bg-brand-blue p-3 rounded-lg text-white">
+              Quick Registration
             </button>
-            <button className="flex gap-10">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-5 h-5 text-yellow-400"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
-                  clipRule="evenodd"
-                />
-              </svg>
+            <button className="flex gap-6 items-center">
+              <div className="p-3 bg-white rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-5 h-5 text-yellow-400"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
               <span>Watch Video</span>
             </button>
           </div>
         </div>
-        <div></div>
+      </div>
+      <div className="bg-gray-300 h-20">
+        <img
+          src="https://tukuz.com/wp-content/uploads/2020/06/emerson-ecologics-logo-vector.png"
+          alt=""
+          className="h-10"
+        />
       </div>
     </div>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,27 @@
 import React from "react";
+import Logo from "../assets/logo.png";
 
 export default function Navbar() {
   return (
-    <div>
-      <div className="flex gap-5 justify-around pt-7">
-        <p>Eatly</p>
-        <ul className="flex gap-8 text-xl">
-          <li>Programs</li>
-          <li>Services</li>
-          <li>News</li>
+    <div className="bg-[#0e1f44] text-white">
+      <div className="flex gap-5 pt-5 items-center justify-center">
+        <img src={Logo} alt="logo" className="h-20" />
+        <ul className="flex gap-8 text-lg">
+          <li className="text-[#0655ff]">Home</li>
+          <li>Category</li>
+          <li>Catelog</li>
           <li>About us</li>
+          <li>Music</li>
+          <li>Stream</li>
         </ul>
-        <button className="bg-yellow-400 p-3 rounded-lg">Let's Talk</button>
+        <div className="flex gap-5">
+          <button className="rounded-3xl border-2 px-5 py-2 border-[#0655ff] hover:bg-[#0655ff]">
+            Login
+          </button>
+          <button className="bg-[#0655ff] rounded-3xl px-5 py-3 hover:bg-transparent hover:border-2 hover:border-[#0655ff] border-2 border-transparent">
+            Register
+          </button>
+        </div>
       </div>
     </div>
   );
