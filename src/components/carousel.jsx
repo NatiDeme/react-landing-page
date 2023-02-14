569f43542f364cff6373138e9e37490693106c1c
NatiDeme <demelashnatnael@hotmail.com>
2023-02-14 16:14:23 +0300

    Add overflow styling to cards

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -8,17 +8,19 @@ import {
 export default function CarouselLayout() {
   let me = [1, 2, 3, 4, 5];
   return (
-    <div className="bg-[#021131] flex flex-col items-center">
-      <div className="flex items-center justify-between pt-5 w-[65rem]">
-        <p className="text-brand-neutral text-2xl font-semibold">
-          Recently released
-        </p>
-        <div className="flex gap-2">
-          <ArrowLeftCircleIcon className="text-brand-yellow h-5" />
-          <ArrowRightCircleIcon className="text-brand-yellow h-5" />
+    <div className="bg-[#021131]">
+      <div className="flex justify-center w-full">
+        <div className="flex items-center justify-between pt-5 w-[65rem]">
+          <p className="text-brand-neutral text-2xl font-semibold">
+            Recently released
+          </p>
+          <div className="flex gap-2">
+            <ArrowLeftCircleIcon className="text-brand-yellow h-5" />
+            <ArrowRightCircleIcon className="text-brand-yellow h-5" />
+          </div>
         </div>
       </div>
-      <div className="flex gap-4 pb-5 overflow-x-auto shrink-0 w-[65rem]">
+      <div className="flex gap-4 pb-5 overflow-x-auto pl-[10%]">
         {me.map((m) => (
           <CarouselCard key={m} />
         ))}
